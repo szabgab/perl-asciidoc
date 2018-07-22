8d685eea0a0e9fe1085eb573c3c159ff92f71661
Gabor Szabo <gabor@szabgab.com>
2018-07-22 17:02:20 +0200

    move dom to be part of the instance

--- lib/Asciidoc/Parser.pm
+++ lib/Asciidoc/Parser.pm
@@ -12,29 +12,29 @@ sub new {
 
 sub parse_file {
     my ($self, $filename) = @_;
-    my %dom;
+    $self->{dom} = {};
 
     open my $fh, '<:encoding(UTF-8)', $filename or die;
     my $in_header;
     my @para;
     while (my $line = <$fh>) {
         if ($line =~ /^---$/) {
-            if (not $dom{header}) {
-                $dom{header} = {};
+            if (not $self->{dom}{header}) {
+                $self->{dom}{header} = {};
                 $in_header = 1;
                 next;
             } else {
                 $in_header = 0;
-                $dom{content} = [];
+                $self->{dom}{content} = [];
                 next;
             }
         }
         if ($in_header and $line =~ /^([^:]*):\s+(.*)$/) {
-            $dom{header}{$1} = $2;
+            $self->{dom}{header}{$1} = $2;
             next;
         }
         if ($line =~ /^(=+)\s+(.*)$/) {
-            push @{$dom{content}}, {
+            push @{$self->{dom}{content}}, {
                 tag => 'h' . length($1),
                 text => $2,
             };
@@ -49,7 +49,7 @@ sub parse_file {
         if ($line =~ /^\s*$/) {
             next if not @para; # before first para?
             if (@para) {
-                push @{$dom{content}}, {
+                push @{$self->{dom}{content}}, {
                     tag => 'p',
                     cont => [ @para ],
                 };
@@ -62,14 +62,14 @@ sub parse_file {
     }
 
     if (@para) {
-        push @{$dom{content}}, {
+        push @{$self->{dom}{content}}, {
             tag => 'p',
             cont => [ @para ],
         };
         @para = ();
     }
 
-    return \%dom;
+    return $self->{dom};
 }
 
 sub parse_line {
