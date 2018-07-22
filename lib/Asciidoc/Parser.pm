7c9a97d09d83a9894eeb20143e70ddf770fa8c71
Gabor Szabo <gabor@szabgab.com>
2018-07-22 07:36:35 +0200

    process header

--- lib/Asciidoc/Parser.pm
+++ lib/Asciidoc/Parser.pm
@@ -12,28 +12,36 @@ sub new {
 
 sub parse_file {
     my ($self, $filename) = @_;
-    my %content;
+    my %dom;
 
     open my $fh, '<:encoding(UTF-8)', $filename or die;
     my $in_header;
     while (my $line = <$fh>) {
         if ($line =~ /^---$/) {
-            if (not $content{header}) {
-                $content{header} = {};
+            if (not $dom{header}) {
+                $dom{header} = {};
                 $in_header = 1;
                 next;
             } else {
                 $in_header = 0;
+                $dom{content} = [];
                 next;
             }
         }
         if ($in_header and $line =~ /^([^:]*):\s+(.*)$/) {
-            $content{header}{$1} = $2;
+            $dom{header}{$1} = $2;
+            next;
+        }
+        if ($line =~ /^(=+)\s+(.*)$/) {
+            push @{$dom{content}}, {
+                tag => 'h' . length($1),
+                text => $2,
+            };
             next;
         }
     }
 
-    return \%content;
+    return \%dom;
 }
 
 1;
