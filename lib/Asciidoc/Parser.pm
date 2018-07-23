bbaa75d607aafec0eb418bae8010cc93f5974407
Gabor Szabo <gabor@szabgab.com>
2018-07-23 22:02:25 +0200

    add spaces

--- lib/Asciidoc/Parser.pm
+++ lib/Asciidoc/Parser.pm
@@ -65,7 +65,8 @@ sub parse_file {
             $self->save_para and next;
         }
 
-        push @{$self->{para}}, $self->parse_line("$line ");
+        push @{$self->{para}}, ' ' if @{ $self->{para} };
+        push @{$self->{para}}, $self->parse_line($line);
     }
 
     $self->save_para;
